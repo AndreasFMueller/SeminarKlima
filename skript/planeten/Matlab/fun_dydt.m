72453d6be3974a2269e5382ba8ea95433a3d0d16
Nicolas Tobler <ntobler@gmail.com>
2018-07-24 21:23:32 +0200

    changed matlab model

--- skript/planeten/Matlab/fun_dydt.m
+++ skript/planeten/Matlab/fun_dydt.m
@@ -10,14 +10,15 @@ function dydt = fun_dydt(t,y)
     H   = y(2);
     C   = y(3); 
     
-    P = pi * PLANET.R^2 * CONST.sigma * CONST.T_sun^4 * (CONST.R_sun / PLANET.A)^2;
-    P_absorption = P / (4* pi * PLANET.R^2); %W/m^2
+    S = CONST.sigma * CONST.T_sun^4 * (CONST.R_sun / PLANET.A)^2;
+    P = pi* PLANET.R^2 * S;
+    S_absorption = P / (4* pi * PLANET.R^2); %W/m^2
     
     P = 4* pi * PLANET.R^2 * CONST.sigma * T_s.^4;
-    P_blackbody  = P / (4* pi * PLANET.R^2); %W/m^2
+    S_blackbody  = P / (4* pi * PLANET.R^2); %W/m^2
     
-    P_in  = P_absorption * (1 - (((PARAM.alpha_max - PARAM.alpha_s) * C) + PARAM.alpha_s) );
-    P_out = P_blackbody  * (1 - ( PARAM.xi8 * H)         );
+    P_in  = S_absorption * (1 - (((PARAM.alpha_max - PARAM.alpha_s) * C) + PARAM.alpha_s) );
+    P_out = S_blackbody  * (1 - ( PARAM.xi8 * H)         );
     
     T_grad = PARAM.xi3 * 1./(C * T_s); 
     
