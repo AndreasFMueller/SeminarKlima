e40458a9e6e46e89fca9ef9a134591fab8fb6761
Andreas Müller <afm@margaret.local>
2018-08-03 11:33:00 +0200

    Beispiel vervollständigt

--- skript/chapters/6/beispiel.m
+++ skript/chapters/6/beispiel.m
@@ -16,26 +16,31 @@ function ret = dreieck(fid, m, name)
 	y(1:m,1) = (1:m) / m;
 	y(m+1:m+n,1) = 1 - (1:n)/m;
 	y(m+n+1:N,1) = (1:m) / m - 1;
-	y = shift(y, 1, 1)
-	ret = fft(y);
+	y = shift(y, 1, 1);
+	ret = fft(y) / n
 	fprintf(fid, "\\def\\%s{\n", name);
 	fprintf(fid, "\\draw[color=red] plot[domain=0:6.283,samples=100] ({\\x},{");
 	for j = (2:2:n)
-		fprintf(fid, "-(%.5f)*sin(%d*\\x*\\anglescale)", imag(ret(j,1)) / n, j - 1)
+		fprintf(fid, "-(%.5f)*sin(%d*\\x*\\anglescale)", imag(ret(j,1)), j - 1)
 	endfor
 	fprintf(fid, "});\n}\n");
 endfunction
 
-dreieck(fid, 1, "eins")
-dreieck(fid, 2, "zwei")
-dreieck(fid, 3, "drei")
-dreieck(fid, 4, "vier")
-dreieck(fid, 5, "fuenf")
-dreieck(fid, 6, "sechs")
-dreieck(fid, 7, "sieben")
-dreieck(fid, 8, "acht")
-dreieck(fid, 10, "zehn")
+dreieck(fid, 1, "eins");
+dreieck(fid, 2, "zwei");
+dreieck(fid, 3, "drei");
+dreieck(fid, 4, "vier");
+dreieck(fid, 5, "fuenf");
+dreieck(fid, 6, "sechs");
+dreieck(fid, 7, "sieben");
+dreieck(fid, 8, "acht");
+dreieck(fid, 10, "zehn");
 dreieck(fid, 20, "zwanzig");
+
+#dreieck(fid, 1000, "tausend");
 
 fclose(fid);
 
+j = (1:6);
+k = 2*j-1;
+8./(pi^2*k.^2)
